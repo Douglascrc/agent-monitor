b345d502868adbeb4e59e08711c08c08d67495ff
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-03-07 16:27:12 -0300

    initial commit

---
+++ src/main/java/com/rnp/agent/monitor/AgentMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.rnp.agent.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rnp/agent/monitor/AgentMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rnp.agent.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
